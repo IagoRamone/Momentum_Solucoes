ba1fdbbca4463438cceb09adf51524530a76cf58
Brendo Silva <142930047+BrendoRSS@users.noreply.github.com>
2025-05-10 20:25:56 -0300

    ajuste de design na tela de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,10 +23,11 @@ export default function LoginPage() {
 
   return (
     <>
-    <header className="w-full bg-[#f1f1f1] border-b border-gray-300 py-2 flex justify-start shadow-sm">
-      <img src="/momentum_logo.png" alt="Logo" className="ml-12 max-w-[100px]" />
+    <header className="w-full bg-[#fff] shadow-xl py-2 flex justify-start">
+    <a href="/login"><img src="/momentum_logo.png" alt="Logo" className="ml-12 max-w-[70px]" /></a>
+      
     </header>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#f8f9fa] p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#f8f9fa] p-4 shadow-xl">
 
       {error && (
         <div className="fixed top-6 right-6 bg-red-500 text-white px-4 py-2 rounded z-50 font-bold">
@@ -34,50 +35,72 @@ export default function LoginPage() {
         </div>
       )}
 
-      <div className="bg-white border border-black rounded-lg mt-16 p-8 w-[300px] shadow-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+      <div className="bg-white border border-none rounded-lg mt-16 p-8 w-[350px] shadow-xl">
+        <h3 className="text-2xl  mb-6 text-center">Login</h3>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <label htmlFor="email" className="text-left text-sm font-medium">
-            Email
-          </label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            maxLength={40}
-            required
-            className="border border-black rounded px-3 py-2"
-          />
+  <div className="relative ">
+    <input
+      type="text"
+      id="email"
+      placeholder=" "
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      maxLength={40}
+      required
+      className="cursor-pointer peer w-full px-2 py-3 text-base border border-gray-300 rounded-md outline-none focus:border-violet-500 inset-shadow-sm"
+    
+    />
+    <label
+      htmlFor="email"
+      className="text-xs absolute left-2 top-3 text-gray-500 text-base transition-all duration-200 
+        peer-placeholder-shown:top-3 peer-placeholder-shown:text-base 
+        peer-focus:top-[-10px] peer-focus:left-2 peer-focus:text-xs 
+        peer-focus:text-violet-500 peer-focus:bg-white peer-focus:px-1"
+    >
+      E-mail
+    </label>
+  </div>
 
-          <label htmlFor="password" className="text-left text-sm font-medium">
-            Senha
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            maxLength={10}
-            required
-            className="border border-black rounded px-3 py-2"
-          />
+  <div className="relative">
+    <input
+      type="password"
+      id="password"
+      placeholder=" "
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      maxLength={10}
+      required
+      className="cursor-pointer peer w-full px-2 py-3 text-base border border-gray-300 rounded-md outline-none focus:border-violet-500 inset-shadow-sm"
+    />
+    <label
+      htmlFor="password"
+      className="text-xs cursor-pointer absolute left-2 top-3 text-gray-500 text-base transition-all duration-200 
+        peer-placeholder-shown:top-3 peer-placeholder-shown:text-base 
+        peer-focus:top-[-10px] peer-focus:left-2 peer-focus:text-xs 
+        peer-focus:text-violet-500 peer-focus:bg-white peer-focus:px-1"
+    >
+      Senha
+    </label>
+  </div>
 
-          <a href="#" className="text-xs text-black font-bold hover:underline">
-            Esqueci minha senha
-          </a>
+  <a href="#" className="text-xs text-gray-600 transition duration-200 hover:underline">
+    <i>Esqueci minha senha</i> 
+  </a>
 
-          <button type="submit" className="bg-white border-2 border-black rounded-full py-2 font-bold hover:bg-black hover:text-white transition">
-            Entrar
-          </button>
+  <button
+  type="submit"
+  className="text-sm cursor-pointer text-center border-2 border-gray-400 text-violet-600 rounded-lg py-2 px-4 transition-colors duration-200 hover:bg-violet-800 hover:text-white"
+>
+  Entrar
+</button>
 
-          <a
-            href="/cadastro"
-            className="btn text-center border-2 border-black rounded-full py-2 font-bold hover:bg-black hover:text-white transition"
-          >
-            Cadastrar
-          </a>
-        </form>
+  <a
+    href="/cadastro"
+    className="text-sm cursor-pointer text-center border-2 border-gray-400 text-violet-600 rounded-lg py-2 px-4 transition-colors duration-200 hover:bg-violet-800 hover:text-white"
+  >
+    Cadastrar
+  </a>
+</form>
       </div>
     </div>
     </>
